721bd15e2e183441698582bb699d46bb698269c9
Coen Hyde <coen.hyde@gmail.com>
2010-04-26 18:15:19 +1000

    Added ArrayAccess to Shanty_Mongo_Connection_Stack

--- library/Shanty/Mongo/Connection/Stack.php
+++ library/Shanty/Mongo/Connection/Stack.php
@@ -7,11 +7,13 @@
  * @license    New BSD License
  * @author     Coen Hyde
  */
-class Shanty_Mongo_Connection_Stack implements SeekableIterator, Countable
+class Shanty_Mongo_Connection_Stack implements SeekableIterator, Countable, ArrayAccess
 {
 	protected $_position = 0;
 	protected $_nodes = array();
 	protected $_weights = array();
+	protected $_cacheConnectionSelection = true;
+	protected $_cachedConnection = null;
 	
 	/**
 	 * Add node to connection stack
@@ -32,18 +34,81 @@ class Shanty_Mongo_Connection_Stack implements SeekableIterator, Countable
 	 */
 	public function selectNode()
 	{
+		if (count($this) == 0) {
+			require_once 'Shanty/Mongo/Exception.php';
+			throw new Shanty_Mongo_Exception("No nodes available for selection");
+		}
+		
+		// Return the cached connection if available
+		if ($this->cacheConnectionSelection() && $this->hasCachedConnection()) {
+			return $this->getCachedConnection();
+		}
+		
+		// Select a new connection
 		$r = mt_rand(1,array_sum($this->_weights));
 		$offset = 0;
 		foreach ($this->_weights as $k => $weight) {
 			$offset += $weight;
 			if ($r <= $offset) {
-				return $this->_nodes[$k];
+				$connection = $this->_nodes[$k];
+				break;
 			}
 		}
+		
+		// Cache the connection for later use
+		if ($this->cacheConnectionSelection()) {
+			$this->_cachedConnection = $connection;
+		}
+		
+		return $connection;
 	}
 	
+	/**
+	 * Determine if this connection stack has a cached connection
+	 * 
+	 * @return boolean
+	 */
+	public function hasCachedConnection()
+	{
+		return !is_null($this->_cachedConnection);
+	}
+	
+	/**
+	 * Get the cached connection
+	 * 
+	 * @return Shanty_Mongo_Connection
+	 */
+	public function getCachedConnection()
+	{
+		return $this->_cachedConnection;
+	}
+	
+	/**
+	 * Get or set the flag to determine if the first connection selection should be cached
+	 * 
+	 * @param boolean $value
+	 */
+	public function cacheConnectionSelection($value = null)
+	{
+		if (!is_null($value)) {
+			$this->_cacheConnectionSelection = (boolean) $value;
+		}
+		
+		return $this->_cacheConnectionSelection;
+	}
+	
+	/**
+	 * Seek to a particular connection
+	 * 
+	 * @param $position
+	 */
 	public function seek($position)
 	{
+		if (!is_numeric($position)) {
+			require_once 'Shanty/Mongo/Exception.php';
+			throw new Shanty_Mongo_Exception("Position must be numeric");
+		}
+		
 		$this->_position = $position;
 		
 		if (!$this->valid()) {
@@ -51,33 +116,105 @@ class Shanty_Mongo_Connection_Stack implements SeekableIterator, Countable
 		}
 	}
 	
+	/**
+	 * Get the current connection
+	 * 
+	 * @return Shanty_Mongo_Connection
+	 */
 	public function current()
 	{
 		return $this->_nodes[$this->_position];
 	}
 	
+	/**
+	 * Get teh current key
+	 * 
+	 * @return int
+	 */
 	public function key()
 	{
 		return $this->_position;
 	}
 	
+	/**
+	 * Move the pointer to the next connection
+	 */
 	public function next()
 	{
 		$this->_position +=1;
 	}
 	
+	/**
+	 * Rewind the pointer to the begining of the stack
+	 */
 	public function rewind()
 	{
 		$this->_position = 0;
 	}
 	
+	/**
+	 * Is the location of the current pointer valid
+	 */
 	public function valid()
 	{
-		return array_key_exists($this->_position, $this->_nodes);
+		return $this->offsetExists($this->_position);
 	}
 	
+	/**
+	 * Count all the connections
+	 */
 	public function count()
 	{
 		return count($this->_nodes);
+	}
+	
+	/**
+	 * Test if an offset exists
+	 * 
+	 * @param int $offset
+	 */
+	public function offsetExists($offset)
+	{
+		return array_key_exists($offset, $this->_nodes);
+	}
+	
+	/**
+	 * Get an offset
+	 * 
+	 * @param int $offset
+	 */
+	public function offsetGet($offset)
+	{
+		if (!$this->offsetExists($offset)) return null;
+		
+		return $this->_nodes[$offset];
+	}
+	
+	/**
+	 * Set an offset
+	 * 
+	 * @param Shanty_Mongo_Connection $offset
+	 * @param $connection
+	 */
+	public function offsetSet($offset, $connection)
+	{
+		if (!is_numeric($offset)) {
+			require_once 'Shanty/Mongo/Exception.php';
+			throw new Shanty_Mongo_Exception("Offset must be numeric");
+		}
+		
+		$this->_nodes[$offset] = $connection;
+		$this->_weights[$offset] = 1;
+	}
+	
+	/**
+	 * Unset an offset
+	 * 
+	 * @param int $offset
+	 */
+	public function offsetUnset($offset)
+	{
+		unset($this->_nodes[$offset]);
+		unset($this->_weights[$offset]);
 	}
 }
--- tests/Shanty/Mongo/Connection/StackTest.php
+++ tests/Shanty/Mongo/Connection/StackTest.php
@@ -2,7 +2,6 @@
 require_once dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
 
 require_once 'PHPUnit/Framework.php';
-require_once 'Shanty/Mongo/Connection/Stack.php';
  
 class Shanty_Mongo_Connection_StackTest extends PHPUnit_Framework_TestCase
 {
@@ -13,7 +12,14 @@ class Shanty_Mongo_Connection_StackTest extends PHPUnit_Framework_TestCase
 		$this->_stack = new Shanty_Mongo_Connection_Stack();
 	}
 	
-	public function testAddNode()
+	public function testCacheConnectionSelection()
+	{
+		$this->assertTrue($this->_stack->cacheConnectionSelection());
+		$this->_stack->cacheConnectionSelection(false);
+		$this->assertFalse($this->_stack->cacheConnectionSelection());
+	}
+	
+	public function testAddAndCountNodes()
 	{
 		$this->assertEquals(0, count($this->_stack));
 		
@@ -24,6 +30,18 @@ class Shanty_Mongo_Connection_StackTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(2, count($this->_stack));
 	}
 	
+	/**
+	 * @expectedException Shanty_Mongo_Exception
+	 */
+	public function testNoNodes()
+	{
+		$this->assertEquals(0, count($this->_stack));
+		$this->_stack->selectNode();
+	}
+	
+	/**
+	 * @depends testAddAndCountNodes
+	 */
 	public function testSelectNode()
 	{
 		$connection = $this->getMock('Shanty_Mongo_Connection');
@@ -31,8 +49,38 @@ class Shanty_Mongo_Connection_StackTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals($connection, $this->_stack->selectNode());
 	}
 	
+	/**
+	 * @depends testCacheConnectionSelection
+	 * @depends testAddAndCountNodes
+	 * @depends testSelectNode
+	 */
+	public function testCacheConnection()
+	{
+		$this->_stack->cacheConnectionSelection(true);
+		
+		$this->_stack->addNode($this->getMock('Shanty_Mongo_Connection'));
+		$this->_stack->addNode($this->getMock('Shanty_Mongo_Connection'));
+		$this->_stack->addNode($this->getMock('Shanty_Mongo_Connection'));
+		$this->_stack->addNode($this->getMock('Shanty_Mongo_Connection'));
+		$this->_stack->addNode($this->getMock('Shanty_Mongo_Connection'));
+		
+		$connection = $this->_stack->selectNode();
+		for ($i = 0; $i < 100; $i++) {
+			if ($this->_stack->selectNode() != $connection) {
+				$this->fail('Connection was not cached property');
+			}
+		} 
+	}
+	
+	/**
+	 * @depends testCacheConnectionSelection
+	 * @depends testAddAndCountNodes
+	 * @depends testSelectNode
+	 */
 	public function testNodeWeight()
 	{
+		$this->_stack->cacheConnectionSelection(false);
+		
 		$nodes = array();
 		$nodes[] = array('connection' => $this->getMock('Shanty_Mongo_Connection'), 'weight' => 100, 'selected' => 0);
 		$nodes[] = array('connection' => $this->getMock('Shanty_Mongo_Connection'), 'weight' => 300, 'selected' => 0);
@@ -64,6 +112,9 @@ class Shanty_Mongo_Connection_StackTest extends PHPUnit_Framework_TestCase
 		}
 	}
 	
+	/**
+	 * @depends testAddAndCountNodes
+	 */
 	public function testNodeIterate()
 	{
 		$this->_stack->addNode($this->getMock('Shanty_Mongo_Connection'));
@@ -86,5 +137,36 @@ class Shanty_Mongo_Connection_StackTest extends PHPUnit_Framework_TestCase
 		
 		$this->assertEquals(0, $this->_stack->key());
 		
+		$this->_stack->seek(2);
+		$this->assertEquals(2, $this->_stack->key());
+		
+	}
+	
+	/**
+	 * @expectedException OutOfBoundsException
+	 */
+	public function testSeekException()
+	{
+		$this->_stack->seek(100);
+	}
+	
+	public function testArrayAccess()
+	{
+		$this->assertFalse($this->_stack->offsetExists(100));
+		
+		$connection1 = $this->getMock('Shanty_Mongo_Connection');
+		$connection2 = $this->getMock('Shanty_Mongo_Connection');
+		$this->_stack->offsetSet(0, $connection1);
+		$this->_stack->offsetSet(1, $connection2);
+		
+		$this->assertEquals(2, count($this->_stack));
+		
+		$this->assertEquals($connection1, $this->_stack->offsetGet(0));
+		$this->assertEquals($connection2, $this->_stack->offsetGet(1));
+		$this->assertNull($this->_stack->offsetGet(2));
+		
+		$this->_stack->offsetUnset(0);
+		$this->assertNull($this->_stack->offsetGet(0));
+		$this->assertEquals(1, count($this->_stack));
 	}
 }
