c266dcc3656d8e4b4dca1599f8e96288ad46e22c
Tom Holder <tom@simpleweb.co.uk>
2012-06-17 17:35:40 +0100

    Added sub document with export

---
+++ tests/Shanty/Mongo/_files/My/ShantyMongo/SimpleSubDocWithExport.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'My/ShantyMongo/Abstract.php';
+
+class My_ShantyMongo_SimpleSubDocWithExport extends My_ShantyMongo_Abstract
+{
+	protected static $_requirements = array(
+		'myIgnoredProperty' => 'Ignore',
+		'subDoc' => array('Document:My_ShantyMongo_SimpleSubDocWithExport')
+	);
+	
+	public function export() {
+		$data = parent::export();
+		$data['myIgnoredProperty'] = 'sub document data';
+		$data['myUnignoredProperty'] = 'sub document data';
+		return $data;
+	}
+}
